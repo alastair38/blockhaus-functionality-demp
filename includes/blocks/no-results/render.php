1f2293701781c36f0fb143fe33fdae19dd3085a5
alastair38 <alastair@alastaircox.com>
2024-03-20 15:42:08 +0000

    ~ no-results text for post-type archives

--- includes/blocks/no-results/render.php
+++ includes/blocks/no-results/render.php
@@ -11,9 +11,9 @@
 
 $noResults = 'No content was found';
 
-if(is_singular('blog-de') || is_singular('place-de') || is_singular('resources-de') || get_post_type($post_id) === 'blog-de' || get_post_type($post_id) === 'place-de' || get_post_type($post_id) === 'resources-de'):
+if(is_singular('blog-de') || is_singular('place-de') || is_singular('resources-de') || get_post_type($post_id) === 'blog-de' || get_post_type($post_id) === 'place-de' || get_post_type($post_id) === 'resources-de' || is_post_type_archive('resources-de' ) || is_post_type_archive('blog-de')):
   $noResults = 'Es wurde kein Inhalt gefunden';
-elseif(is_singular('blog-fr') || is_singular('place-fr') || is_singular('resources-fr') || get_post_type($post_id) === 'blog-fr' || get_post_type($post_id) === 'place-fr' || get_post_type($post_id) === 'resources-fr'):
+elseif(is_singular('blog-fr') || is_singular('place-fr') || is_singular('resources-fr') || get_post_type($post_id) === 'blog-fr' || get_post_type($post_id) === 'place-fr' || get_post_type($post_id) === 'resources-fr' || is_post_type_archive('resources-fr' ) || is_post_type_archive('blog-fr')):
   $noResults = "Aucun contenu n'a été trouvé";
 else:
   $noResults = 'No content was found';
