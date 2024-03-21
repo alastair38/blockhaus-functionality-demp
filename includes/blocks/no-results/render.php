8344aabd387aa5d12e5b43a5be50efd8405135d4
alastair38 <alastair@alastaircox.com>
2024-03-21 14:34:16 +0000

    ~ text and styling for no-results block

--- includes/blocks/no-results/render.php
+++ includes/blocks/no-results/render.php
@@ -9,14 +9,14 @@
  * @param   (int|string) $post_id The post ID this block is saved to.
  */
 
-$noResults = 'No content was found';
+$noResults = 'No content is currently available. Please check back shortly.';
 
 if(is_singular('blog-de') || is_singular('place-de') || is_singular('resources-de') || get_post_type($post_id) === 'blog-de' || get_post_type($post_id) === 'place-de' || get_post_type($post_id) === 'resources-de' || is_post_type_archive('resources-de' ) || is_post_type_archive('blog-de')):
-  $noResults = 'Es wurde kein Inhalt gefunden';
+  $noResults = 'Derzeit ist kein Inhalt verfügbar. Bitte schauen Sie in Kürze wieder vorbei';
 elseif(is_singular('blog-fr') || is_singular('place-fr') || is_singular('resources-fr') || get_post_type($post_id) === 'blog-fr' || get_post_type($post_id) === 'place-fr' || get_post_type($post_id) === 'resources-fr' || is_post_type_archive('resources-fr' ) || is_post_type_archive('blog-fr')):
-  $noResults = "Aucun contenu n'a été trouvé";
+  $noResults = "Aucun contenu n'est actuellement disponible. Veuillez vérifier à nouveau sous peu.";
 else:
-  $noResults = 'No content was found';
+  $noResults = 'No content is currently available. Please check back shortly.';
 endif;
 
 echo '<p>' . $noResults . '</p>';
